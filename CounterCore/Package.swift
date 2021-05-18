80981a8ed14d7083fc84e2db27a31e411f9e4d01
Thales Frigo <thalesfrigo@gmail.com>
2021-05-18 17:14:43 -0300

    Create Dependencies for  Remote and local implementations.

--- CounterCore/Package.swift
+++ CounterCore/Package.swift
@@ -1,28 +1,47 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "CounterCore",
+    name: "Counters",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "CounterCore",
-            targets: ["CounterCore"]),
+        .library(name: "CounterCore", targets: ["CounterCore"]),
+        .library(name: "CounterAPI", targets: ["CounterAPI"]),
+        .library(name: "CounterStore", targets: ["CounterStore"]),
+        .library(name: "HTTPClient", targets: ["HTTPClient"]),
+        .library(name: "URLSessionHTTPClient", targets: ["URLSessionHTTPClient"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "CounterCore",
-            dependencies: []),
+        .target(name: "CounterCore"),
         .testTarget(
             name: "CounterCoreTests",
-            dependencies: ["CounterCore"]),
+            dependencies: ["CounterCore"]
+        ),
+        .target(
+            name: "CounterAPI",
+            dependencies: ["CounterCore"]
+        ),
+        .testTarget(
+            name: "CounterAPITests",
+            dependencies: ["CounterAPI"]
+        ),
+        .target(
+            name: "CounterStore",
+            dependencies: ["CounterCore"]
+        ),
+        .testTarget(
+            name: "CounterStoreTests",
+            dependencies: ["CounterStore"]
+        ),
+        .target(name: "HTTPClient"),
+        .target(
+            name: "URLSessionHTTPClient",
+            dependencies: ["HTTPClient"]
+        ),
+        .testTarget(
+            name: "URLSessionHTTPClientTests",
+            dependencies: ["URLSessionHTTPClient"]
+        ),
     ]
 )
