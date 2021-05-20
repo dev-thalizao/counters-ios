f1ec2639e903898c95f4bd02504d6bc9ed7b1126
Thales Frigo <thalesfrigo@gmail.com>
2021-05-19 21:19:55 -0300

    Implementation of CounterPresenter and CountersViewModel.

--- CounterCore/Sources/CounterPresentation/CounterPresenter.swift
+++ CounterCore/Sources/CounterPresentation/CounterPresenter.swift
@@ -6,7 +6,53 @@
 //
 
 import Foundation
+import CounterCore
+
+struct CountersViewModel: Equatable {
+    
+    let counters: [CounterViewModel]
+    
+    var summary: String {
+        guard !counters.isEmpty else { return "" }
+        
+        var summary = [String]()
+        
+        summary.append("\(counters.count)")
+        counters.count > 1
+            ? summary.append("items")
+            : summary.append("item")
+
+        let total = counters
+            .map(\.count)
+            .compactMap(Int.init)
+            .reduce(0, +)
+        
+        summary.append("· Counted \(total)")
+        
+        total > 1
+            ? summary.append("times")
+            : summary.append("time")
+        
+        return summary.joined(separator: " ")
+    }
+}
+
+struct CounterViewModel: Equatable {
+    let count: String
+    let title: String
+}
 
 final class CounterPresenter {
     static let title = "Counters"
+    
+    static func map(_ counters: [Counter]) -> CountersViewModel {
+        return CountersViewModel(
+            counters: counters.map { counter in
+                CounterViewModel(
+                    count: counter.count.description,
+                    title: counter.title
+                )
+            }
+        )
+    }
 }
--- CounterCore/Tests/CounterPresentationTests/CounterPresenterTests.swift
+++ CounterCore/Tests/CounterPresentationTests/CounterPresenterTests.swift
@@ -6,11 +6,48 @@
 //
 
 import XCTest
+import CounterCore
 @testable import CounterPresentation
 
 final class CounterPresenterTests: XCTestCase {
     
     func testPresenterTitle() {
         XCTAssertEqual(CounterPresenter.title, "Counters")
+    }
+    
+    func testMapCreatesViewModels() {
+        let counters = [
+            Counter(id: "xvaf", title: "Cups of coffee", count: 5),
+            Counter(id: "gfdg", title: "Records played", count: 10),
+            Counter(id: "rtgc", title: "Apples eaten", count: 0),
+        ]
+        
+        let viewModel = CounterPresenter.map(counters)
+        
+        XCTAssertEqual(viewModel.summary, "3 items · Counted 15 times")
+        XCTAssertEqual(viewModel.counters, [
+            CounterViewModel(count: "5", title: "Cups of coffee"),
+            CounterViewModel(count: "10", title: "Records played"),
+            CounterViewModel(count: "0", title: "Apples eaten")
+        ])
+    }
+    
+    func testMapOfEmptyViewModelsCreateEmptySummary() {
+        let viewModel = CounterPresenter.map([])
+        XCTAssertTrue(viewModel.summary.isEmpty)
+    }
+    
+    func testMapOfOneViewModelsCreateSingleSummary() {
+        let viewModel = CounterPresenter.map([
+            Counter(id: "asdf", title: "Apples eaten", count: 1)
+        ])
+        XCTAssertEqual(viewModel.summary, "1 item · Counted 1 time")
+    }
+    
+    func testMapOfOneViewModelsCreateMiscSummary() {
+        let viewModel = CounterPresenter.map([
+            Counter(id: "asdf", title: "Apples eaten", count: 2)
+        ])
+        XCTAssertEqual(viewModel.summary, "1 item · Counted 2 times")
     }
 }
