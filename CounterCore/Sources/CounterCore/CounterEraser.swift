85c8fcabdfb9745c2f1da67a1635e533ba8dcc1d
Thales Frigo <thalesfrigo@gmail.com>
2021-05-26 11:48:29 -0300

    Create CounterEraser

---
+++ CounterCore/Sources/CounterCore/CounterEraser.swift
@@ -0,0 +1,14 @@
+//
+//  CounterEraser.swift
+//  
+//
+//  Created by Thales Frigo on 26/05/21.
+//
+
+import Foundation
+
+public protocol CounterEraser {
+    typealias Result = Swift.Result<Void, Error>
+    
+    func erase(_ id: Counter.ID, completion: @escaping (Result) -> Void)
+}
